abc3a7aa66fa3ac341a82ab086b1848a9949eb3c
lr <466088269@qq.com>
2019-08-04 21:54:51 +0800

    git上传

--- src/main/java/com/demo/Dish.java
+++ src/main/java/com/demo/Dish.java
@@ -1,4 +1,27 @@
 package com.demo;
 
 public class Dish {
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    public Dish(String name, boolean vegetarian, int calories) {
+        this.name = name;
+        this.vegetarian = vegetarian;
+        this.calories = calories;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+    public int getCalories() {
+        return calories;
+    }
+
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
